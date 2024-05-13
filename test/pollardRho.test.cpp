0456023fa520ff961523dcf1c1cc13c175e883ec
Kazuki-115 <c.kazu0315@icloud.com>
2024-05-13 16:26:15 +0900

    pollardRho

---
+++ test/pollardRho.test.cpp
@@ -0,0 +1,40 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/factorize"
+
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll =long long;
+#define all(v) v.begin(),v.end()
+ #define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+
+#include "../math/PollardRho.hpp"
+
+
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+  cin.tie(0);
+
+ll Q;cin>>Q;
+
+PollardRho::init();
+
+for(ll i=0;i<Q;i++) {
+  ll a;cin>>a;
+  auto ans=PollardRho::factorize(a);
+  sort(all(ans));
+  
+
+  cout<<ans.size()<<" ";
+  for(auto x:ans) cout<<x<<" ";
+  cout<<endl;
+}
+}
+
+
+
+
+
